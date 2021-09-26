137ddfabebc788a2a8f87e15056f5a6acc18bc0f
chaptsand <chaptsand@gmail.com>
2021-09-26 23:17:28 +0800

    cas: Initial device tree bringup
    * fstab was taken from LA.UM.9.12.r1-12800-SMxx50.0
    
    Change-Id: I3218f7ad746a0dad80e890ba5eb65444ba7b63c1

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_cas.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_cas-eng \
+    lineage_cas-user \
+    lineage_cas-userdebug
