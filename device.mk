8c5fac7b9c4b1cf9c20a5e56de00ed1d65f008d9
Alexander Koskovich <zvnexus@outlook.com>
2021-09-27 23:36:26 +0800

    cas: Import vendor WiFi & framework overlays.
     * From LA.UM.9.12.r1-12500-SMxx50.0
    
    Change-Id: I3d426d23fe8aa5d4c1c535b772fc426125fae715

--- device.mk
+++ device.mk
@@ -28,9 +28,11 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
+    FrameworksResTarget \
     SystemUIResCommon \
     TelephonyResCommon \
     WifiResCommon \
+    WifiResTarget \
     DeviceFrameworks \
     DeviceSystemUI
 
