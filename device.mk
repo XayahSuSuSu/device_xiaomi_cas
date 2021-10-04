7a8c5b9c83282670d02ce585b1e1ae498cf996a5
Xayah <zds1249475336@gmail.com>
2021-10-04 12:05:40 +0800

    cas: vibrator: Switch to QTI vibrator
    Change-Id: I5f30fa2d5be9585e51fc2c33aa7890616ca7565e

--- device.mk
+++ device.mk
@@ -394,7 +394,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_kona
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
