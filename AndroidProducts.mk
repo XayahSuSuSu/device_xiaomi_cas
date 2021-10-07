fec516e61c845349d080958aebc046c0471ee51a
Xayah <zds1249475336@gmail.com>
2021-10-07 13:53:23 +0800

    cas: Arrow ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cas.mk
+    $(LOCAL_DIR)/arrow_cas.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cas-eng \
-    lineage_cas-user \
-    lineage_cas-userdebug
+    arrow_cas-eng \
+    arrow_cas-user \
+    arrow_cas-userdebug
--- arrow_cas.mk
+++ arrow_cas.mk
@@ -11,14 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit from the Lineage configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from the ArrowOS configuration.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := lineage_cas
+PRODUCT_NAME := arrow_cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
 PRODUCT_MANUFACTURER := Xiaomi
+
+DEVICE_MAINTAINER := Xayah
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
