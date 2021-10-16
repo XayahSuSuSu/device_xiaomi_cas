5bcfd376be99c3c60f5cd43299e9ef042059c890
Xayah <zds1249475336@gmail.com>
2021-10-16 17:38:27 +0800

    [DNM] cas: Disable boot jars
    Change-Id: I72f3212b03445f7a82767b153471bc5db729644b

--- device.mk
+++ device.mk
@@ -201,8 +201,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+#PRODUCT_BOOT_JARS += \
+#    org.ifaa.android.manager
 
 # Init
 PRODUCT_COPY_FILES += \
@@ -426,5 +426,5 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
