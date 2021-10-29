6d2f8b5165ab02b41b5394410cd523e82eac7d43
Kuber Sharma <kubersharma001@gmail.com>
2021-10-30 00:16:44 +0800

    cas: Ship GCamGOPrebuilt instead of Camera2/Snap
     * works with/without GAPPS.

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# CameraGO
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
