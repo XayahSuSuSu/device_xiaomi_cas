ccaaa22b8217398a79763b5da1455f941a86a284
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-11 17:58:52 +0800

    cas: parts: Implement dc dimming
    Squash of the following commits:
    - parts: Move dc dimming to Settings > Display
    - parts: Implement dc dimming preference
    - parts: Disable dc dimming if it's not supported
    - parts: Add DC-Dimming QS tile
    - sepolicy: Address DC Dimming denial
    
    Change-Id: I048371e23c68eae199b429169866af864cc03daf
    Co-Authored-By: Arian <arian.kulmer@web.de>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,20 +20,31 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
 import android.util.Log;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/class/drm/card0-DSI-1/disp_param";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
+
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "0x40000" : "0x50000");
     }
 }
