0f3af2f9a0d3c2df11f28c1a01ae9aece7fadcdb
XayahSuSuSu <zds1249475336@gmail.com>
2021-12-19 13:29:11 +0800

    cas: parts: Drop Dirac
    Change-Id: Ib91ced619e5605588d8c3023b11c8d8721848ee5

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,7 +25,6 @@ import android.os.SystemProperties;
 import android.util.Log;
 import androidx.preference.PreferenceManager;
 
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.utils.FileUtils;
 
@@ -41,7 +40,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
