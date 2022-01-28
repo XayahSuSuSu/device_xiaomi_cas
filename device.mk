92127e4ee6a51cfdefd229176103e7b0a848f391
XayahSuSuSu <zds1249475336@gmail.com>
2022-01-28 16:26:20 +0800

    Revert "[DNM] cas: Disable boot jars"
    This reverts commit a0cc1a0dff5d5f6e72be48d00698229e0d52d820.
    
    Change-Id: I1905f07283801b9481c9e8c6c2363b76cab13fbd

--- device.mk
+++ device.mk
@@ -213,8 +213,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-#PRODUCT_BOOT_JARS += \
-#    org.ifaa.android.manager
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
 
 # Init
 PRODUCT_COPY_FILES += \
