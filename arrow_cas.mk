016aa9db65ccf41466ade83e3f5618dd83a8fac0
Xayah <zds1249475336@gmail.com>
2023-01-03 19:03:23 +0800

    cas: Update blobs to V13.0.6.0.SJJCNXM
    Change-Id: Iaefb5d2864c105ab67d6d4d9b8b9d8cbc4b132e5

--- arrow_cas.mk
+++ arrow_cas.mk
@@ -24,4 +24,4 @@ DEVICE_MAINTAINER := Xayah
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.1.0.SJJCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cas/cas:12/RKQ1.211001.001/V13.0.6.0.SJJCNXM:user/release-keys
